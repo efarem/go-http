4ad9d668e3a64ca7691d4d3b2f004e3981e7a405
Frank Martin <frank.martin@digital-results.com>
2017-01-17 13:47:05 +0000

    Formatting and constant for XSecret header

--- server.go
+++ server.go
@@ -1,34 +1,38 @@
 package main
 
 import (
-  "net/http"
-  "github.com/labstack/echo"
+	"github.com/labstack/echo"
+	"net/http"
 )
 
 type (
-  Data struct {
-    Secret string `json:"secret"`
-  }
+	Data struct {
+		Secret string `json:"secret"`
+	}
+)
+
+const (
+	HeaderXSecret = "X-Secret"
 )
 
 func main() {
 
-  e := echo.New()
+	e := echo.New()
 
-  e.POST("/hello/:name", func(c echo.Context) error {
+	e.POST("/hello/:name", func(c echo.Context) error {
 
-    d := new(Data)
+		d := new(Data)
 
-    if err := c.Bind(d); err != nil {
-      return err
-    }
+		if err := c.Bind(d); err != nil {
+			return err
+		}
 
-    c.Response().Header().Set("X-Secret", d.Secret)
+		c.Response().Header().Set(HeaderXSecret, d.Secret)
 
-    name := c.Param("name")
+		name := c.Param("name")
 
-    return c.String(http.StatusOK, "<h1>Hello " + name + "</h1>\n")
-  })
+		return c.String(http.StatusOK, "<h1>Hello "+name+"</h1>\n")
+	})
 
-  e.Logger.Fatal(e.Start(":1337"))
+	e.Logger.Fatal(e.Start(":1337"))
 }
